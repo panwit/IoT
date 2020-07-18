cfff6ff4cd0b2ab357257bc0d52a5cfcd6cf12cc
panwit <panwit@it.kmitl.ac.th>
2020-07-19 01:58:22 +0700

    Add files via upload

---
+++ netpie/netpie_sub.ino
@@ -0,0 +1,80 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "Your ssid";
+const char* password = "Your password";
+const char* mqtt_server = "broker.netpie.io";
+const int mqtt_port = 1883;
+const char* mqtt_Client = "Your Client ID";
+const char* mqtt_username = "Your Token";
+const char* mqtt_password = "Your Secret";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+int value = 0;
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection…");
+    if (client.connect(mqtt_Client, mqtt_username, mqtt_password)) {
+      Serial.println("connected");
+      client.subscribe("@msg/led");
+    }
+    else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println("try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+    Serial.print("Message arrived [");
+    Serial.print(topic);
+    Serial.print("] ");
+    String message;
+    for (int i = 0; i < length; i++) {
+        message = message + (char)payload[i];
+    }
+    Serial.println(message);
+    if(String(topic) == "@msg/led") {
+        if(message == "on"){
+            digitalWrite(D0,LOW);
+            Serial.println("LED on");
+        }
+        else if (message == "off"){
+            digitalWrite(D0,HIGH);
+            Serial.println("LED off");
+        }
+    }
+}
+
+
+void setup() {
+  pinMode(D0,OUTPUT);
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  
+}
